5fdadeb886a132aa9d26403a1848d7c500e3c663
방우현 <bread@bang-uhyeon-ui-MacBookAir.local>
2023-07-09 01:02:46 +0900

    first commit

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+import {Body, Post} from "@nestjs/common";
+export class CreateUserDto {
+    name: string;
+    email: string;
+}
+
---
+++ src/users/users.controller.ts
@@ -0,0 +1,59 @@
+import {Controller, Get, Post, Body, Patch, Param, Delete, Res, BadRequestException} from '@nestjs/common';
+import { Redirect} from "@nestjs/common";
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { GetUserDto} from "./dto/get-user.dto";
+import { HttpCode} from "@nestjs/common";
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.usersService.create(createUserDto);
+  // }
+  @Post()
+  create(@Body() createUserDto: CreateUserDto){
+    const {name, email} = createUserDto;
+    return `유저를 생성했습니다. 이름: ${name}, 이메일: ${email}`;
+  }
+
+
+  @Get()
+  findAll(@Res() res) {
+    const users = this.usersService.findAll();
+    console.log(users);
+    return res.status(200).send(users)
+  }
+
+  @Redirect('https://naver.com',301)
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (+id<1){
+      throw new BadRequestException('id는 0보다 큰 값이어야 한다')
+    }
+    return this.usersService.findOne(+id);
+  }
+
+  @HttpCode(202)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+
+  @Delete(':userId/memo/:memoId')
+  deleteUserMemo(
+      @Param('userId') userId: string,
+      @Param('memoId') memoId: string,
+  ){
+    return `userId: ${userId}, memoId: ${memoId}`;
+  }
+}
+
