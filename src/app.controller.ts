f500e5bd1f059c5375853710d075c9aadb3e11a2
방우현 <bread@bang-uhyeon-ui-MacBookAir.local>
2023-07-10 01:35:19 +0900

    23.07.10(mondey) 103 page Done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,14 @@ import {CommonService} from "./common.service";
 
 @Controller()
 export class AppController{
-    constructor(private readonly commonService: CommonService) {
-    }
-    @Get('/common-hello')
-    getCommonHello(): string{
-        return this.commonService.hello();
+    // constructor(private readonly commonService: CommonService) {
+    // }
+    // @Get('/common-hello')
+    // getCommonHello(): string{
+    //     return this.commonService.hello();
+    // }
+    @Get()
+    getHello(): string{
+        return process.env.DATABASE_HOST;
     }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import {UsersModule} from "./users/users.module";
 import {EmailModule} from "./email/email.module";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {CommonModule} from "./common.module";
 
+// @Module({
+//   imports: [UsersModule, EmailModule],
+//   controllers: [],
+//   providers: []
+// })
+// export class AppModule { }
 
 @Module({
-  imports: [UsersModule, EmailModule],
-  controllers: [],
-  providers: []
+  imports: [ConfigModule.forRoot({
+    envFilePath: (process.env.NODE_ENV === 'production')? '.production.env'
+        :(process.env.NODE_ENV ==='stage')? '.stage.env': '.development.env'
+  })],
+  controllers: [AppController],
+  providers: [AppService,ConfigService]
 })
 export class AppModule { }
